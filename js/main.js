d704b8c3bbebb781851c9fd799e05e5659457386
marlenakub <marlena.kubicka@wp.pl>
2022-01-03 11:58:22 +0100

    33-js-wyswietlanie-opisu-skonczone

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let myDescription = document.getElementById("opis");
+
+let myButton = document.querySelector("button");
+
+
+myButton.onclick = function() 
+{
+    myDescription.textContent = "Dawno, dawno temu...";
+}
